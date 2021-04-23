6ac3da3b867234fe17252935a91dd56f1d3decb1
tischenkod <43536099+tischenkod@users.noreply.github.com>
2021-04-23 13:04:55 +0300

    Lun algorithm implemented

--- src/banking/Card.java
+++ src/banking/Card.java
@@ -3,37 +3,58 @@ package banking;
 import java.util.Random;
 
 public class Card {
-    String number;
-    String pin;
+    int[] number;
+    int[] pin;
 
     public Card() {
+        number = new int[] {4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        pin = new int[4];
         Random random = new Random();
-        StringBuilder sb = new StringBuilder("400000");
-        for (int i = 0; i < 10; i++) {
-            sb.append(random.nextInt(10));
+        for (int i = 0; i < 9; i++) {
+            number[i + 6] = random.nextInt(10);
         }
-        number = sb.toString();
-        sb = new StringBuilder();
+        number[15] = lun();
         for (int i = 0; i < 4; i++) {
-            sb.append(random.nextInt(10));
+            pin[i] = random.nextInt(10);
         }
-        pin = sb.toString();
+    }
+
+    private int lun() {
+        int sum = 0;
+        int tmp;
+        for (int i = 0; i < 15; i++) {
+            tmp = i % 2 == 0 ? number[i] * 2 : number[i];
+            if (tmp > 9) {
+                tmp -= 9;
+            }
+            sum += tmp;
+        }
+        sum %= 10;
+        return sum == 0 ? 0 : 10 - sum;
     }
 
     public String getPin() {
-        return pin;
+        return concat(pin);
+    }
+
+    private String concat(int[] array) {
+        StringBuilder sb = new StringBuilder();
+        for (int item: array) {
+            sb.append(item);
+        }
+        return sb.toString();
     }
 
     public String getNumber() {
-        return number;
+        return concat(number);
     }
 
     @Override
     public String toString() {
         return "Your card has been created\n" +
                 "Your card number:\n" +
-                number +
+                getNumber() +
                 "\nYour card PIN:\n" +
-                pin;
+                getPin();
     }
 }
